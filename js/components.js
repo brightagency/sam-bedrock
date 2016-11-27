9dce93ac6afb911b7c82a5cb37719a31a6bc9f90
Sam Davis <sam@sjd.co>
2016-11-28 09:40:49 +1100

    Move jQuery to php loaded

--- js/components.js
+++ js/components.js
@@ -4,7 +4,6 @@
 |--------------------------------------------------------------------------
 */
 
-// @codekit-prepend: '../bower_components/jquery/dist/jquery.js'
 // @codekit-prepend: '../bower_components/what-input/what-input.js'
 // @codekit-prepend: '../bower_components/foundation-sites/dist/foundation.js'
 // @codekit-prepend: '../bower_components/matchHeight/jquery.matchHeight.js'
