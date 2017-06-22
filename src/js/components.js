719de0d8b56349e2a925975a1af6a89382f16b19
TreeTrum <sam@sjd.co>
2017-06-22 16:45:02 +1000

    Only include subset of foundation js

--- src/js/components.js
+++ src/js/components.js
@@ -6,6 +6,9 @@
 
 window.$ = jQuery;
 
+// @codekit-prepend: '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.js'
+// @codekit-prepend: '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js'
+// @codekit-prepend: '../../node_modules/foundation-sites/dist/js/plugins/foundation.interchange.js'
+
 // @codekit-prepend: '../../node_modules/what-input/dist/what-input.js'
-// @codekit-prepend: '../../node_modules/foundation-sites/dist/js/foundation.js'
 // @codekit-prepend: '../../node_modules/jquery-match-height/dist/jquery.matchHeight.js'
