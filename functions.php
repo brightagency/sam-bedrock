acce9a7693b1aad8e126de63ea6c457188c6926d
TreeTrum <sam@sjd.co>
2017-06-27 16:04:18 +1000

    Rename components.js to vendor.js

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ function bedrock_scripts_styles() {
 	// SCRIPTS
 
     // User scripts
-    wp_enqueue_script( 'components', get_template_directory_uri() . '/dist/js/components-min.js?v=' . md5_file(get_template_directory() . '/dist/js/components-min.js'), ['jquery'], null, true );
+    wp_enqueue_script( 'vendor', get_template_directory_uri() . '/dist/js/vendor-min.js?v=' . md5_file(get_template_directory() . '/dist/js/vendor-min.js'), ['jquery'], null, true );
     wp_enqueue_script( 'app', get_template_directory_uri() . '/dist/js/app-min.js?v=' . md5_file(get_template_directory() . '/dist/js/app-min.js'), ['jquery'], null, true );
 
 	// Pass WordPress data into our JS
