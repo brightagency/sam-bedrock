ff51de938af9b35cadfd52722e411591ad4ed302
TreeTrum <sam@sjd.co>
2017-10-03 12:37:07 +1100

    Account for iPhone X adjustments

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name='viewport' content='initial-scale=1, viewport-fit=cover'>
 
     <!--[if lt IE 9]>
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
