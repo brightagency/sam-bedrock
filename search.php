11f549e4709c0738b7ee0d6b581cafdb00028b5a
Sam Davis <samjd@me.com>
2018-02-19 11:12:28 +1100

    Update search.php

--- search.php
+++ search.php
@@ -1,25 +1,69 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : ?>
+<section class="page-content">
 
-    <h2>Search Results: <?php echo get_search_query(); ?></h2>
+    <div class="grid-container">
+        <div class="grid-x grid-padding-x align-center">
+            <div class="medium-8 large-10 cell">
 
-    <?php while ( have_posts() ) : the_post(); ?>
+                <div class="search-list">
 
-    <article <?php post_class(); ?>>
+                    <p class="found"><?php echo "Found " . $wp_query->found_posts . " results for \"" . get_search_query() . "\"" ?></p>
 
-    	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    <?php if ( have_posts() ) : ?>
+                        <?php global $wp_query; ?>
 
-    	<?php the_excerpt(); ?>
+                        <?php while ( have_posts() ) : the_post(); ?>
 
-    </article>
+                            <article <?php post_class('search-result'); ?>>
 
-    <?php endwhile; ?>
+                                <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
 
-<?php else: ?>
+                                <p class="dateline">
+                                    <?php echo get_post_type_labels( get_post_type_object( get_post(get_the_id())->post_type ) )->singular_name; ?>
+                                    &nbsp;|&nbsp;
+                                    Last updated <?php the_time('jS F Y'); ?>
+                                </p>
 
-    <p>No results found.</p>
+                                <?php the_excerpt(); ?>
 
-<?php endif; ?>
+                                <p class="permalink">
+                                    <a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a>
+                                </p>
+
+                            </article>
+
+                        <?php endwhile; ?>
+
+                        <div class="pagination">
+                            <?php
+                            global $wp_query;
+
+                            $big = 999999999; // need an unlikely integer
+
+                            echo paginate_links( array(
+                                'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                                'format' => '?paged=%#%',
+                                'current' => max( 1, get_query_var('paged') ),
+                                'total' => $wp_query->max_num_pages,
+                                'prev_text' => '<i class="far fa-chevron-left"></i>',
+                                'next_text' => '<i class="far fa-chevron-right"></i>'
+                            ) );
+                            ?>
+                        </div>
+
+                    <?php else: ?>
+
+                        <?php // No search results ?>
+
+                    <?php endif; ?>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
 
 <?php get_footer(); ?>
